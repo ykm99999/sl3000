786e2e0f670759461a85ee20468a6d56ff14cf56
ykm888 <165190180+ykm888@users.noreply.github.com>
2025-12-21 01:29:35 +0800

    Update mt7981.mk

--- target/linux/mediatek/image/mt7981.mk
+++ target/linux/mediatek/image/mt7981.mk
@@ -1,20 +1,17 @@
-define Device/sl3000-emmc
-  DEVICE_VENDOR := SL
+define Device/sl3000_emmc
+  DEVICE_VENDOR := Siluoxing
   DEVICE_MODEL := SL3000
   DEVICE_VARIANT := eMMC
   DEVICE_DTS := mt7981b-sl3000-emmc
   DEVICE_DTS_DIR := $(DTS_DIR)/mediatek
-  SUPPORTED_DEVICES := sl3000-emmc
-  BLOCKSIZE := 128k
-  PAGESIZE := 2048
-  KERNEL_SIZE := 4096k
-  IMAGE_SIZE := 120832k
 
-  IMAGES += sysupgrade.bin
+  DEVICE_PACKAGES := \
+        kmod-mt7915e kmod-mt7981-firmware \
+        kmod-mt7996e kmod-mt7996-firmware \
+        kmod-mmc kmod-mmc-mtk kmod-spi-dev \
+        uboot-envtools block-mount
+
+  IMAGES := sysupgrade.bin
   IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
-
-  DEVICE_PACKAGES := kmod-usb3 kmod-usb2 kmod-leds-gpio kmod-gpio-button-hotplug \
-	kmod-mt7915e kmod-mt7981-firmware mt7981-wo-firmware \
-	wpad-basic-mbedtls
 endef
-TARGET_DEVICES += sl3000-emmc
+TARGET_DEVICES += sl3000_emmc
