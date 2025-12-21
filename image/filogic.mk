4a90ac379843d0df6f1e901045b6b1c26a7f46f7
ykm888 <165190180+ykm888@users.noreply.github.com>
2025-12-21 18:46:28 +0800

    Update filogic.mk

--- image/filogic.mk
+++ image/filogic.mk
@@ -1,21 +1,14 @@
 define Device/sl3000_emmc
-  DEVICE_VENDOR := SL
-  DEVICE_MODEL := 3000
-  DEVICE_VARIANT := eMMC
+  DEVICE_TITLE := SL3000 eMMC
   DEVICE_DTS := mt7981b-sl3000-emmc
+  DTS_DIR := mediatek/
   DEVICE_PACKAGES := \
-	kmod-mt7915e \
-	kmod-mt7981-firmware \
-	mt7981-wo-firmware \
-	kmod-mediatek_hnat \
-	kmod-mt7531 \
-	block-mount \
-	e2fsprogs \
-	kmod-fs-ext4 \
-	kmod-mmc \
-	kmod-mmc-mtk \
-	kmod-nft-offload \
-	kmod-nft-core \
-	kmod-nft-nat
+        kmod-mt7981-firmware \
+        kmod-mt7915e \
+        kmod-mt7531 \
+        swconfig \
+        kmod-usb3 \
+        kmod-usb2 \
+        kmod-usb-ledtrig-usbport
 endef
 TARGET_DEVICES += sl3000_emmc
