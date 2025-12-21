933324ffc1ed50cda543dd6e3ad157e3f86418bb
ykm888 <165190180+ykm888@users.noreply.github.com>
2025-12-21 10:35:11 +0800

    Update filogic.mk

--- image/filogic.mk
+++ image/filogic.mk
@@ -1,21 +1,21 @@
 define Device/sl3000_emmc
-  DEVICE_VENDOR := Siluoxing
-  DEVICE_MODEL := SL3000
+  DEVICE_VENDOR := SL
+  DEVICE_MODEL := 3000
   DEVICE_VARIANT := eMMC
   DEVICE_DTS := mt7981b-sl3000-emmc
-  DEVICE_DTS_DIR := $(DTS_DIR)/mediatek
-
   DEVICE_PACKAGES := \
-        kmod-mt7915e kmod-mt7981-firmware \
-        kmod-mt7996e kmod-mt7996-firmware \
-        kmod-mmc kmod-mmc-mtk kmod-spi-dev \
-        uboot-envtools block-mount
-
-  KERNEL := kernel-bin | fit
-  KERNEL_SUFFIX := -recovery.itb
-  KERNEL_INITRAMFS_SUFFIX := -initramfs-recovery.itb
-
-  IMAGES := sysupgrade.itb
-  IMAGE/sysupgrade.itb := sysupgrade-tar | append-metadata
+	kmod-mt7915e \
+	kmod-mt7981-firmware \
+	mt7981-wo-firmware \
+	kmod-mediatek_hnat \
+	kmod-mt7531 \
+	block-mount \
+	e2fsprogs \
+	kmod-fs-ext4 \
+	kmod-mmc \
+	kmod-mmc-mtk \
+	kmod-nft-offload \
+	kmod-nft-core \
+	kmod-nft-nat
 endef
 TARGET_DEVICES += sl3000_emmc
